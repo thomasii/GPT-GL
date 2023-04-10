fc49cdbe5a1b512196f6028517ea3602f9264a63
Thomas Wright <thomaswright1318@gmail.com>
2023-04-10 14:06:29 -0400

    ambient Lighting

--- fragmentShader.js
+++ fragmentShader.js
@@ -2,19 +2,21 @@ export const fragmentShaderSource = `#version 300 es
 precision mediump float;
 
 in vec3 v_position;
-in vec3 v_normal;
 in vec3 v_lightDirection;
 
 uniform vec3 u_lightColor;
 uniform float u_lightIntensity;
+uniform float u_ambientIntensity; // Add this line
 
 out vec4 outColor;
 
 void main() {
-  vec3 normal = normalize(v_normal);
+  vec3 normal = normalize(cross(dFdx(v_position), dFdy(v_position)));
   float light = max(0.0, dot(normal, v_lightDirection));
-  vec3 color = u_lightColor * light * u_lightIntensity;
+  vec3 directLight = u_lightColor * light * u_lightIntensity;
 
-  outColor = vec4(color, 1.0);
+  vec3 ambientLight = u_lightColor * u_ambientIntensity; // Add this line
+
+  outColor = vec4(directLight + ambientLight, 1.0); // Modify this line
 }
 `;
